a5f2b5ffa235d9f4c11418c321814b1455fa584f
Michael Nightingale <mnightingale@eon-media.com>
2022-03-22 15:17:47 +0000

    Fix not ignoring empty push collection

--- resources/views/initialize.blade.php
+++ resources/views/initialize.blade.php
@@ -1,5 +1,5 @@
 <script type="application/javascript" async src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id={{app(\EonVisualMedia\LaravelKlaviyo\KlaviyoClient::class)->getPublicKey()}}"></script>
-@if(!empty($data))
+@if($data->isNotEmpty())
 <script type="application/javascript">
 var _learnq = window._learnq || [];
 @foreach($data as $item)
